780344d5db2a39652a3d745bc5476fddaefec0d4
Shaz2518 <shaziyashaikh2518@outlook.com>
2026-08-13 22:06:48 +0530

    Ecommerce- Add to Cart

--- SeleniumRevision/src/e2ePracticeProject/FlightBookingAutomation.java
+++ SeleniumRevision/src/e2ePracticeProject/FlightBookingAutomation.java
@@ -15,55 +15,51 @@ public class FlightBookingAutomation {
 		driver.manage().window().maximize();
 
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		
-		//Autosuggestion Dropdown
+
+		// Autosuggestion Dropdown
 		driver.findElement(By.id("autosuggest")).sendKeys("Uni");
 		Thread.sleep(1000);
 		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item']"));
-		for(WebElement option: options)
-		{
+		for (WebElement option : options) {
 			String country = option.getText();
-			if(country.equalsIgnoreCase("United States (USA)"))
-			{
+			if (country.equalsIgnoreCase("United States (USA)")) {
 				option.click();
 			}
 		}
-	    System.out.println(driver.findElement(By.id("autosuggest")).getText());
-		
-		//Select On-way trip and check if return date checkpoint is disabled
-	    driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0")).click();
-	    if(driver.findElement(By.id("Div1")).getDomAttribute("style").contains("1"))
-	    		{
-	    			System.out.println("Return Date is Enabled");
-	    		}
-	    else
-	    	System.out.println("Return date is Disabled");
-	    
-	    //Select From and To destination
-	    driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-	    Thread.sleep(1000);
-	    driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='PNQ']")).click();
-	    Thread.sleep(1000);
-	    driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='GOI']")).click();
-	 
-	    //Select current date, 5 Adults and USD Currency and Select family and friends checkpoint and click on search
-	    driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();  //Current date
-	    driver.findElement(By.id("divpaxinfo")).click();
-	    Thread.sleep(1000);
-	    for(int i=1; i<5; i++)
-	    {
-	    	driver.findElement(By.id("hrefIncAdt")).click(); //5 Adults
-	    }
-	    driver.findElement(By.id("btnclosepaxoption")).click();
-	    WebElement selectOption = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")); //USD Currency
-	    Select dropdownOption = new Select(selectOption);
-	    dropdownOption.selectByValue("USD");
-	    
-	    driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
-	    driver.findElement(By.cssSelector("input[name='ctl00$mainContent$btn_FindFlights']")).click();
-	    
-	    
-		   
+		System.out.println(driver.findElement(By.id("autosuggest")).getText());
+
+		// Select On-way trip and check if return date checkpoint is disabled
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0")).click();
+		if (driver.findElement(By.id("Div1")).getDomAttribute("style").contains("1")) {
+			System.out.println("Return Date is Enabled");
+		} else
+			System.out.println("Return date is Disabled");
+
+		// Select From and To destination
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='PNQ']"))
+				.click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='GOI']"))
+				.click();
+
+		// Select current date, 5 Adults and USD Currency and Select family and friends
+		// checkpoint and click on search
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click(); // Current date
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(1000);
+		for (int i = 1; i < 5; i++) {
+			driver.findElement(By.id("hrefIncAdt")).click(); // 5 Adults
+		}
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		WebElement selectOption = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")); // USD Currency
+		Select dropdownOption = new Select(selectOption);
+		dropdownOption.selectByValue("USD");
+
+		driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+		driver.findElement(By.cssSelector("input[name='ctl00$mainContent$btn_FindFlights']")).click();
+
 	}
 
 }
---
+++ SeleniumRevision/src/ecommerceWebsite/SingleProduct_AddToCart.java
@@ -0,0 +1,32 @@
+package ecommerceWebsite;
+import java.util.Arrays;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SingleProduct_AddToCart {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		String[] productNames = { "Cucumber", "Carrot", "Potato", "Onion" };
+		driver.get("https://rahulshettyacademy.com/seleniumPractise");
+		Thread.sleep(1000);
+		int j=0;
+
+		List<WebElement> productList = driver.findElements(By.xpath("//h4[@class='product-name']"));
+		for (int i = 0; i < productList.size(); i++) {
+			String allProducts = productList.get(i).getText();
+			String finalProduct = allProducts.split(" ")[0];
+			// System.out.println(finalProduct);
+			List<String> productNeeded = Arrays.asList(productNames);
+			if (productNeeded.contains(finalProduct)) {
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				if(j==productNames.length)
+					break;
+				}
+		}
+	}
+}
