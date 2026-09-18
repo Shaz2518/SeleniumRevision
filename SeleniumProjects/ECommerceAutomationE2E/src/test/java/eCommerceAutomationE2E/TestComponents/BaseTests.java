3456c9dfb8fe06d00684ecd1a0575948ab3f95e9
Shaz2518 <shaziyashaikh2518@outlook.com>
2026-09-18 21:56:55 +0530

    Headless Browser

--- SeleniumProjects/ECommerceAutomationE2E/src/test/java/eCommerceAutomationE2E/TestComponents/BaseTests.java
+++ SeleniumProjects/ECommerceAutomationE2E/src/test/java/eCommerceAutomationE2E/TestComponents/BaseTests.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -51,9 +52,13 @@ public class BaseTests {
 		} else if (browserName.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-		} else if (browserName.equalsIgnoreCase("edge")) {
+		} else if (browserName.contains("edge")) {
+			EdgeOptions options = new EdgeOptions();
+			options.addArguments("headless");
+			if(browserName.contains("headless")) {
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
+			driver.manage().window().setSize(new Dimension(1440,900)); //Default full screen
 		}
 		System.out.println("Selected Browser = " + browserName);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
