9342e97eb51da4abeb7e4634974e0a0f320d6156
Shaz2518 <shaziyashaikh2518@outlook.com>
2026-09-23 21:32:45 +0530

    File Download

--- SeleniumRevision/src/autoITPractice/UploadFile.java
+++ SeleniumRevision/src/autoITPractice/UploadFile.java
@@ -1,10 +1,13 @@
 package autoITPractice;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -18,5 +21,11 @@ public class UploadFile {
 		driver.findElement(By.cssSelector(".uploader__btn")).click();
 		
 		Runtime.getRuntime().exec("C:\\Users\\localadminuser\\Desktop\\SeleniumPractice\\AutoDownload.exe");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("processTask")));
+		driver.findElement(By.id("processTask")).click();
+		
+		
+		
 	}
 }
